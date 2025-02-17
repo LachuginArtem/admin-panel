0837d6c419a54bedb6725089eef9b2d2e7eb4115
Artem Lachugin <lavtemik@mail.ru>
2025-02-18 01:58:18 +0300

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import { FaHome, FaUsers, FaCog, FaSignInAlt } from 'react-icons/fa'; // Импортируем иконки
-import './Sidebar.css'; // Для стилизации боковой панели
+import { FaHome, FaUsers } from 'react-icons/fa';
+import './Sidebar.css';
+import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+function Sidebar() {
   return (
     <div className="sidebar">
       <ul>
-        <li title="Главная"><FaHome className="icon" /></li>
-        <li title="Пользователи"><FaUsers className="icon" /></li>
-        <li title="Настройки"><FaCog className="icon" /></li>
-        <li title="Выход"><FaSignInAlt className="icon" /></li>
+        <li>
+          <Link to="/" className="sidebar-link">
+            <FaHome className="icon" title="Главная" />
+            <span className="tooltip">Главная</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/users" className="sidebar-link">
+            <FaUsers className="icon" title="Пользователи" />
+            <span className="tooltip">Пользователи</span>
+          </Link>
+        </li>
       </ul>
     </div>
   );
-};
+}
 
 export default Sidebar;
+
+//import React from 'react';
+//import { FaHome, FaUsers } from 'react-icons/fa'; // Импортируем иконки
+//import './Sidebar.css'; // Для стилизации боковой панели
+//import { Link } from 'react-router-dom';
+
+//function Sidebar() {
+  //return (
+    //<div className="sidebar">
+      //<ul>
+        //<li>
+          //<Link to="/">
+            //<FaHome /> {/* Иконка для главной страницы */}
+            //Главная
+          //</Link>
+        //</li>
+        //<li>
+          //<Link to="/users">
+            //<FaUsers /> {/* Иконка для пользователей */}
+            //Пользователи
+          //</Link>
+        //</li>
+        //{/* Добавьте другие ссылки с иконками, если нужно */}
+      //</ul>
+    //</div>
+  //);
+//}
+
+//export default Sidebar;
